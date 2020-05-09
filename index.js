2b9550518099d7707a3069e71590c45c0687ad7e
Allen <developer2019@icloud.com>
2020-05-09 01:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let newArr = [];
+
+  for (let i = 0; i < musicians.length; i++){
+    let name = musicians[i];
+    let instrument = instruments[i];
+
+    newArr.push(`${name} plays ${instrument}`);
+  }
+  return newArr;
+}
